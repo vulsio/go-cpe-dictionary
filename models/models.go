8a2abc6050c2f9b95467fac736110e452b5a7a3b
MaineK00n <dev.pylori1229@gmail.com>
2021-07-07 02:12:52 +0900

    feat(rdb): specify the size

--- models/models.go
+++ models/models.go
@@ -4,18 +4,18 @@ package models
 // https://cpe.mitre.org/specification/CPE_2.3_for_ITSAC_Nov2011.pdf
 type CategorizedCpe struct {
 	ID              int64  `json:"-"`
-	CpeURI          string `gorm:"unique;index:idx_categorized_cpe_cpe_uri"`
-	CpeFS           string
-	Part            string
-	Vendor          string `gorm:"index:idx_categorized_cpe_vendor"`
-	Product         string `gorm:"index:idx_categorized_cpe_product"`
-	Version         string
-	Update          string
-	Edition         string
-	Language        string
-	SoftwareEdition string
-	TargetSoftware  string
-	TargetHardware  string
-	Other           string
+	CpeURI          string `gorm:"unique;size:255;index:idx_categorized_cpe_cpe_uri"`
+	CpeFS           string `gorm:"size:255"`
+	Part            string `gorm:"size:255"`
+	Vendor          string `gorm:"size:255;index:idx_categorized_cpe_vendor"`
+	Product         string `gorm:"size:255;index:idx_categorized_cpe_product"`
+	Version         string `gorm:"size:255"`
+	Update          string `gorm:"size:255"`
+	Edition         string `gorm:"size:255"`
+	Language        string `gorm:"size:255"`
+	SoftwareEdition string `gorm:"size:255"`
+	TargetSoftware  string `gorm:"size:255"`
+	TargetHardware  string `gorm:"size:255"`
+	Other           string `gorm:"size:255"`
 	Deprecated      bool
 }
