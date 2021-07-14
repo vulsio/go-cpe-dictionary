66c423cff9c4be4966b301d74b703986f458c8b2
MaineK00n <dev.pylori1229@gmail.com>
2021-07-15 08:56:00 +0900

    feat(fetcher): do not use goroutine for convert

--- fetcher/nvd.go
+++ fetcher/nvd.go
@@ -21,16 +21,13 @@ import (
 // CpeDictionary has cpe-item list
 // https://nvd.nist.gov/cpe.cfm
 type CpeDictionary struct {
-	Items []CpeDictionaryItem `xml:"cpe-item"`
-}
-
-// CpeDictionaryItem :
-type CpeDictionaryItem struct {
-	Name       string `xml:"name,attr"`
-	Deprecated string `xml:"deprecated,attr"`
-	Cpe23Item  struct {
-		Name string `xml:"name,attr"`
-	} `xml:"cpe23-item"`
+	Items []struct {
+		Name       string `xml:"name,attr"`
+		Deprecated string `xml:"deprecated,attr"`
+		Cpe23Item  struct {
+			Name string `xml:"name,attr"`
+		} `xml:"cpe23-item"`
+	} `xml:"cpe-item"`
 }
 
 // V3Feed : NvdV3Feed
@@ -39,15 +36,12 @@ type V3Feed struct {
 	CVEItems []struct {
 		Configurations struct {
 			Nodes []struct {
-				Cpe []V3FeedCpe `json:"cpe_match"`
+				Cpe []struct {
+					Cpe23URI string `json:"cpe23Uri"`
+				} `json:"cpe_match"`
 			} `json:"nodes"`
 		} `json:"configurations"`
 	} `json:"CVE_Items"`
-}
-
-// V3FeedCpe :
-type V3FeedCpe struct {
-	Cpe23URI string `json:"cpe23Uri"`
 }
 
 // FetchNVD NVD feeds
@@ -110,7 +104,7 @@ func FetchCpeDictionary() ([]models.CategorizedCpe, error) {
 	}
 
 	var cpes []models.CategorizedCpe
-	if cpes, err = convertNvdCpeDictionaryToModel(cpeDictionary, viper.GetInt("threads"), viper.GetInt("wait")); err != nil {
+	if cpes, err = convertNvdCpeDictionaryToModel(cpeDictionary); err != nil {
 		return nil, err
 	}
 
@@ -131,7 +125,7 @@ func FetchJSONFeed() ([]models.CategorizedCpe, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Failed to get feeds. err : %s", err)
 	}
-	cpes, err := convertNvdV3FeedToModel(nvds, viper.GetInt("threads"), viper.GetInt("wait"))
+	cpes, err := convertNvdV3FeedToModel(nvds)
 	if err != nil {
 		return nil, err
 	}
@@ -207,62 +201,8 @@ func fetchNVDFeedFile(url string) (nvd *V3Feed, err error) {
 }
 
 // convertNvdCpeDictionaryToModel :
-func convertNvdCpeDictionaryToModel(nvd CpeDictionary, concurrency, wait int) (cpes []models.CategorizedCpe, err error) {
-	blockItems := [][]CpeDictionaryItem{}
-	for i := 0; i < len(nvd.Items); i += concurrency {
-		end := i + concurrency
-		if len(nvd.Items) < end {
-			end = len(nvd.Items)
-		}
-		blockItems = append(blockItems, nvd.Items[i:end])
-	}
-
-	reqChan := make(chan []CpeDictionaryItem, len(nvd.Items))
-	resChan := make(chan []models.CategorizedCpe, len(nvd.Items))
-	errChan := make(chan error)
-	defer close(reqChan)
-	defer close(resChan)
-	defer close(errChan)
-
-	go func() {
-		for _, item := range blockItems {
-			reqChan <- item
-		}
-	}()
-
-	tasks := util.GenWorkers(concurrency, wait)
-	for range blockItems {
-		tasks <- func() {
-			req := <-reqChan
-			cpes, err := convertNvdCpeDictionary(req)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			resChan <- cpes
-		}
-	}
-
-	errs := []error{}
-	timeout := time.After(10 * 60 * time.Second)
-	for range blockItems {
-		select {
-		case res := <-resChan:
-			cpes = append(cpes, res...)
-		case err := <-errChan:
-			errs = append(errs, err)
-		case <-timeout:
-			return nil, fmt.Errorf("Timeout Converting")
-		}
-	}
-	if 0 < len(errs) {
-		return nil, fmt.Errorf("%s", errs)
-	}
-	return cpes, nil
-}
-
-func convertNvdCpeDictionary(items []CpeDictionaryItem) (cpes []models.CategorizedCpe, err error) {
-	for _, item := range items {
+func convertNvdCpeDictionaryToModel(nvd CpeDictionary) (cpes []models.CategorizedCpe, err error) {
+	for _, item := range nvd.Items {
 		var wfn common.WellFormedName
 		if wfn, err = naming.UnbindFS(item.Cpe23Item.Name); err != nil {
 			// Logging only
@@ -290,92 +230,34 @@ func convertNvdCpeDictionary(items []CpeDictionaryItem) (cpes []models.Categoriz
 }
 
 // convertNvdV3FeedToModel :
-func convertNvdV3FeedToModel(nvds []V3Feed, concurrency, wait int) (cpes []models.CategorizedCpe, err error) {
-	allCpes := []V3FeedCpe{}
+func convertNvdV3FeedToModel(nvds []V3Feed) (cpes []models.CategorizedCpe, err error) {
 	for _, nvd := range nvds {
 		for _, item := range nvd.CVEItems {
 			for _, node := range item.Configurations.Nodes {
-				allCpes = append(allCpes, node.Cpe...)
+				for _, cpe := range node.Cpe {
+					var wfn common.WellFormedName
+					if wfn, err = naming.UnbindFS(cpe.Cpe23URI); err != nil {
+						log15.Warn("Failed to unbind cpe.", "CPE URI", cpe.Cpe23URI, "err", err)
+						continue
+					}
+					cpes = append(cpes, models.CategorizedCpe{
+						CpeURI:          naming.BindToURI(wfn),
+						CpeFS:           naming.BindToFS(wfn),
+						Part:            wfn.GetString(common.AttributePart),
+						Vendor:          wfn.GetString(common.AttributeVendor),
+						Product:         wfn.GetString(common.AttributeProduct),
+						Version:         wfn.GetString(common.AttributeVersion),
+						Update:          wfn.GetString(common.AttributeUpdate),
+						Edition:         wfn.GetString(common.AttributeEdition),
+						Language:        wfn.GetString(common.AttributeLanguage),
+						SoftwareEdition: wfn.GetString(common.AttributeSwEdition),
+						TargetSoftware:  wfn.GetString(common.AttributeTargetSw),
+						TargetHardware:  wfn.GetString(common.AttributeTargetHw),
+						Other:           wfn.GetString(common.AttributeOther),
+					})
+				}
 			}
 		}
-	}
-
-	blockItems := [][]V3FeedCpe{}
-	for i := 0; i < len(allCpes); i += concurrency {
-		end := i + concurrency
-		if len(allCpes) < end {
-			end = len(allCpes)
-		}
-		blockItems = append(blockItems, allCpes[i:end])
-	}
-
-	reqChan := make(chan []V3FeedCpe, len(allCpes))
-	resChan := make(chan []models.CategorizedCpe, len(allCpes))
-	errChan := make(chan error)
-	defer close(reqChan)
-	defer close(resChan)
-	defer close(errChan)
-
-	go func() {
-		for _, item := range blockItems {
-			reqChan <- item
-		}
-	}()
-
-	tasks := util.GenWorkers(concurrency, wait)
-	for range blockItems {
-		tasks <- func() {
-			req := <-reqChan
-			cpes, err := convertNvdV3Feed(req)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			resChan <- cpes
-		}
-	}
-
-	errs := []error{}
-	timeout := time.After(10 * 60 * time.Second)
-	for range blockItems {
-		select {
-		case res := <-resChan:
-			cpes = append(cpes, res...)
-		case err := <-errChan:
-			errs = append(errs, err)
-		case <-timeout:
-			return nil, fmt.Errorf("Timeout Converting")
-		}
-	}
-	if 0 < len(errs) {
-		return nil, fmt.Errorf("%s", errs)
-	}
-	return cpes, nil
-}
-
-// convertNvdV3Feed :
-func convertNvdV3Feed(v3FeedCpes []V3FeedCpe) (cpes []models.CategorizedCpe, err error) {
-	for _, cpe := range v3FeedCpes {
-		var wfn common.WellFormedName
-		if wfn, err = naming.UnbindFS(cpe.Cpe23URI); err != nil {
-			log15.Warn("Failed to unbind cpe.", "CPE URI", cpe.Cpe23URI, "err", err)
-			continue
-		}
-		cpes = append(cpes, models.CategorizedCpe{
-			CpeURI:          naming.BindToURI(wfn),
-			CpeFS:           naming.BindToFS(wfn),
-			Part:            wfn.GetString(common.AttributePart),
-			Vendor:          wfn.GetString(common.AttributeVendor),
-			Product:         wfn.GetString(common.AttributeProduct),
-			Version:         wfn.GetString(common.AttributeVersion),
-			Update:          wfn.GetString(common.AttributeUpdate),
-			Edition:         wfn.GetString(common.AttributeEdition),
-			Language:        wfn.GetString(common.AttributeLanguage),
-			SoftwareEdition: wfn.GetString(common.AttributeSwEdition),
-			TargetSoftware:  wfn.GetString(common.AttributeTargetSw),
-			TargetHardware:  wfn.GetString(common.AttributeTargetHw),
-			Other:           wfn.GetString(common.AttributeOther),
-		})
 	}
 	return cpes, nil
 }
