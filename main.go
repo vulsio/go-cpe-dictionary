d3f7e6f900c4761f1257181023af680cefb76406
MaineK00n <mainek00n.1229@gmail.com>
2021-08-20 00:36:15 +0900

    feat(cmd): add version command

--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/vulsio/go-cpe-dictionary/commands"
-	"github.com/vulsio/go-cpe-dictionary/config"
 )
 
 func main() {
-	var v = flag.Bool("v", false, "Show version")
-
 	if envArgs := os.Getenv("GO_CPE_DICTIONARY_ARGS"); 0 < len(envArgs) {
 		commands.RootCmd.SetArgs(strings.Fields(envArgs))
-	} else {
-		flag.Parse()
-	}
-
-	if *v {
-		fmt.Printf("go-cpe-dictionary %s %s\n", config.Version, config.Revision)
-		os.Exit(0)
 	}
 
 	if err := commands.RootCmd.Execute(); err != nil {
